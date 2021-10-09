e919251a1dd7c9ebc0032d2a05f22dd62c972089
jyotip101 <jyotipatelfyit77100@gmail.com>
2021-10-09 23:23:15 +0530

    day 1

--- src/quiz/AlgorithmQuizApp.java
+++ src/quiz/AlgorithmQuizApp.java
@@ -1,18 +1,28 @@
 package quiz;
 
-import java.awt.Color;
-
+import java.awt.*; 
 import javax.swing.*;
 
 public class AlgorithmQuizApp extends JFrame {
-
+ 
+	private static final long serialVersionUID = 1L;
+	
 	AlgorithmQuizApp(){
-		setBounds(200, 200, 1000, 500);
+		setBounds(200, 200, 800, 500);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //		black getContentPane().setBackground(new Color(43, 50, 56));
 //		green getContentPane().setBackground(new Color(137, 172, 88));
 		getContentPane().setBackground(new Color(255, 255, 255));
+		setLayout(null);
+		
+		ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("img/image.jpg"));
+		
+		JLabel iconLabel = new JLabel(icon);
+		iconLabel.setBounds(0, 0, 400, 500);
+		add(iconLabel);
+		
+		JLabel
 		
 		setResizable(false);
 	}
